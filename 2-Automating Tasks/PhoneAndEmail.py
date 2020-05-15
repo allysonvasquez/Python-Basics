1ee8f94c7e8855a3d5d020588c9dccb17948b423
Allyson Vasquez <allysonvasquez01@gmail.com>
2020-05-15 11:41:53 -0400

    PhoneAndEmail.py skeleton

---
+++ 2-Automating Tasks/PhoneAndEmail.py
@@ -0,0 +1,26 @@
+# author: Allyson Vasquez
+# version: May.15.2020
+# Practice Exercises: Regular Expressions
+
+# https://www.w3resource.com/python-exercises/re/index.php
+
+# TODO: check that a string contains only a certain set of characters(a-z, A-Z and 0-9)
+
+
+# TODO: match a string that contains only upper and lowercase letters, numbers, and underscores
+
+# TODO: remove leading zeros from an IP address
+
+# TODO: convert a date from yyyy-mm-dd to mm-dd-yyyy
+
+# TODO: separate and print the numbers of a given string
+
+# TODO: abbreviate Road as Rd. in a home address
+
+# TODO: replace all spaces with a -
+
+# TODO: find all words that are 5 characters long in a string
+
+# TODO: extract values between quotation marks in a string
+
+# TODO: remove all the .com from a list of websites
