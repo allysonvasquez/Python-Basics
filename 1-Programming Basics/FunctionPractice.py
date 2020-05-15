4a9e277bc540f3e02ba697e54587d2df606e5917
Allyson Vasquez <allysonvasquez01@gmail.com>
2020-05-14 20:27:33 -0400

    completed TODO's

--- 1-Programming Basics/FunctionPractice.py
+++ 1-Programming Basics/FunctionPractice.py
@@ -24,19 +24,51 @@ def sum(x):
         total += x[i]
     return total
 
-# TODO: Calculate the factorial of a number
+# Calculate the factorial of a number
+def factorial(x):
+    fac = 1
+    if x == 0:
+        return 0
+    elif x == 1:
+        return 1
 
-# TODO: Check if a number is in a given range
+    while x > 0:
+        fac *= x
+        x -= 1
+    return fac
 
-# TODO: Calculate number of upper/lowercase letters in a string
+# Check if a number is in a given range
+def inRange(x, min, max):
+    if x in range(min, max):
+        print(x, 'is in range between', min, max)
+    else:
+        print(x, 'is NOT range between', min, max)
 
-# TODO: Determine if a string is a palindrome
+# Calculate number of upper/lowercase letters in a string
+def caseCount(x):
+    upper = 0
+    lower = 0
+    for i in x:
+        if i.isupper():
+            upper += 1
+        elif i.islower():
+            lower += 1
+    print('String inputted:', x)
+    print('\tUppercase letters:', upper)
+    print('\tLowercase letters:', lower)
 
 def main():
     print('Max of 3 numbers:', max(112, 18, 43))
 
     my_list = [32, 84, 12, 23, 62]
     print('Sum of numbers in a list:', sum(my_list))
+
+    print(factorial(10))
+
+    inRange(5, 1, 10)
+    inRange(12, 31, 43)
+
+    caseCount('My name is Allyson Vasquez')
 
 if __name__ == '__main__':
     main()
